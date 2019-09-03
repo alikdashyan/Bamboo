fdca7dfffde8eaaba9061d7d48c097390d613f0b
davk01 <davitwjob@gmail.com>
2019-09-03 14:32:09 +0400

    order

--- public/js/controller/config.js
+++ public/js/controller/config.js
@@ -124,13 +124,17 @@ app.controller('authCtrl', function($scope, $http, $location) {
     $scope.submit = function(){
 
       let email = $scope.email;
-        let password = $scope.password;
-        let body = JSON.stringify({
+      let password = $scope.password;
+      let body = JSON.stringify({
           email,
           password
-        });
-
-        $http.post('http://www.amzbamboo.com/users/login', body).then(
+      });
+      const httpOptions = {
+        header: {
+          'Authorization': `Bearer ${localStorage.token}`
+        }
+      }
+        $http.post('http://www.amzbamboo.com/users/login', body,httpOptions).then(
             success => {
               let token = success.data.token;
               if(token){
@@ -161,6 +165,7 @@ app.controller('authCtrl', function($scope, $http, $location) {
           email,
           password,
         }
+        
         let stringifiedBody = JSON.stringify(regBody);
         let checked = document.querySelector('label[for="terms"]');
         let submitBUtton = document.querySelector('input[disabled="disabled"]');
@@ -168,8 +173,13 @@ app.controller('authCtrl', function($scope, $http, $location) {
         if(password != $scope.passwordError ){
           regRequest();
         }
+        const httpOptions = {
+          header: {
+            'Authorization': `Bearer ${localStorage.token}`
+          }
+        }
         function regRequest() {
-          $http.post('http://www.amzbamboo.com/users/signup',stringifiedBody).then(
+          $http.post('http://www.amzbamboo.com/users/signup',stringifiedBody,httpOptions).then(
             success => {
               let token = success.data.token;
               if(token){
@@ -209,6 +219,14 @@ app.controller('postCtrl', function ($scope, $routeParams,postsFactory) {
 })
 
 app.controller('formCntrl', function($scope, $http,$location) {
+  if(!localStorage.token){
+    $location.path('/').replace();
+  }
+  const httpOptions = {
+    header: {
+      'Authorization': `Bearer ${localStorage.token}`
+    }
+  }
   $scope.submitOrder = function(){
     let productLink = $scope.productLink;
     let buyingsPerDay = $scope.buyingsPerDay;
@@ -224,7 +242,7 @@ app.controller('formCntrl', function($scope, $http,$location) {
       additionalInfo,
     }
     let stringifiedBody = JSON.stringify(body);
-    $http.post('http://www.amzbamboo.com/orders',stringifiedBody).then(
+    $http.post('http://www.amzbamboo.com/order',stringifiedBody, httpOptions).then(
       success => {
         let token = success.data.token;
               if(token){
@@ -242,8 +260,16 @@ app.controller('formCntrl', function($scope, $http,$location) {
   }
  
 })
-app.controller('tableCtrl', function($scope,$http){
-  $http.get('http://www.amzbamboo.com/orders').then(
+app.controller('tableCtrl', function($scope,$http,$location){
+  if(!localStorage.token){
+    $location.path('/').replace();
+  }
+  const httpOptions = {
+    header: {
+      'Authorization': `Bearer ${localStorage.token}`
+    }
+  }
+  $http.get('http://www.amzbamboo.com/orders', httpOptions).then(
     success => {
       console.log(success);
     },
@@ -320,280 +346,6 @@ app.controller('HeaderCtrl', function($scope){
      {
        "id": "3",
        "name": "Unlimited Ways"
-     },
-        {
-       "id": "4",
-       "name": "Developer Life"
-     },
-        {
-       "id": "5",
-       "name": "The Blue Sky"
-     },
-         {
-       "id": "6",
-       "name": "Night Life"
-     },
-         {
-       "id": "7",
-       "name": "Another World Perspective"
-     },
-         {
-       "id": "8",
-       "name": "Alone on the Forest"
-     },
-     {
-      "id": "9",
-      "name": "The Blue Sky"
-    },
-        {
-      "id": "10",
-      "name": "Night Life"
-    },
-        {
-      "id": "11",
-      "name": "Another World Perspective"
-    },
-        {
-      "id": "12",
-      "name": "Alone on the Forest"
-    },
-    {
-      "id": "13",
-      "name": "Unlimited Ways"
-    },
-       {
-      "id": "14",
-      "name": "Developer Life"
-    },
-       {
-      "id": "15",
-      "name": "The Blue Sky"
-    },
-        {
-      "id": "16",
-      "name": "Night Life"
-    },
-        {
-      "id": "17",
-      "name": "Another World Perspective"
-    },
-        {
-      "id": "18",
-      "name": "Alone on the Forest"
-    },
-    {
-     "id": "19",
-     "name": "The Blue Sky"
-   },
-       {
-     "id": "20",
-     "name": "Night Life"
-   },
-       {
-     "id": "21",
-     "name": "Another World Perspective"
-   },
-       {
-     "id": "22",
-     "name": "Alone on the Forest"
-   }, {
-    "id": "23",
-    "name": "Unlimited Ways"
-  },
-     {
-    "id": "24",
-    "name": "Developer Life"
-  },
-     {
-    "id": "25",
-    "name": "The Blue Sky"
-  },
-      {
-    "id": "26",
-    "name": "Night Life"
-  },
-      {
-    "id": "27",
-    "name": "Another World Perspective"
-  },
-      {
-    "id": "28",
-    "name": "Alone on the Forest"
-  },
-  {
-   "id": "29",
-   "name": "The Blue Sky"
- },
- {
-  "id": "30",
-  "name": "Unlimited Ways"
-},
-   {
-  "id": "31",
-  "name": "Developer Life"
-},
-   {
-  "id": "32",
-  "name": "The Blue Sky"
-},
-    {
-  "id": "33",
-  "name": "Night Life"
-},
-    {
-  "id": "34",
-  "name": "Another World Perspective"
-},
-    {
-  "id": "35",
-  "name": "Alone on the Forest"
-},
-{
- "id": "36",
- "name": "The Blue Sky"
-},
-{
-  "id": "37",
-  "name": "Amazing Mountain"
-},
-{
-  "id": "38",
-  "name": "Creative Business"
-},
-{
-  "id": "39",
-  "name": "Unlimited Ways"
-},
-   {
-  "id": "40",
-  "name": "Developer Life"
-},
-   {
-  "id": "41",
-  "name": "The Blue Sky"
-},
-    {
-  "id": "42",
-  "name": "Night Life"
-},
-    {
-  "id": "43",
-  "name": "Another World Perspective"
-},
-    {
-  "id": "44",
-  "name": "Alone on the Forest"
-},
-{
- "id": "45",
- "name": "The Blue Sky"
-},
-   {
- "id": "46",
- "name": "Night Life"
-},
-   {
- "id": "47",
- "name": "Another World Perspective"
-},
-   {
- "id": "48",
- "name": "Alone on the Forest"
-},
-{
- "id": "49",
- "name": "Unlimited Ways"
-},
-  {
- "id": "50",
- "name": "Developer Life"
-},
-  {
- "id": "51",
- "name": "The Blue Sky"
-},
-   {
- "id": "52",
- "name": "Night Life"
-},
-   {
- "id": "53",
- "name": "Another World Perspective"
-},
-   {
- "id": "54",
- "name": "Alone on the Forest"
-},
-{
-"id": "55",
-"name": "The Blue Sky"
-},
-  {
-"id": "56",
-"name": "Night Life"
-},
-  {
-"id": "57",
-"name": "Another World Perspective"
-},
-  {
-"id": "58",
-"name": "Alone on the Forest"
-}, {
-"id": "59",
-"name": "Unlimited Ways"
-},
-{
-"id": "60",
-"name": "Developer Life"
-},
-{
-"id": "61",
-"name": "The Blue Sky"
-},
- {
-"id": "62",
-"name": "Night Life"
-},
- {
-"id": "63",
-"name": "Another World Perspective"
-},
- {
-"id": "64",
-"name": "Alone on the Forest"
-},
-{
-"id": "65",
-"name": "The Blue Sky"
-},
-{
-"id": "67",
-"name": "Unlimited Ways"
-},
-{
-"id": "68",
-"name": "Developer Life"
-},
-{
-"id": "69",
-"name": "The Blue Sky"
-},
-{
-"id": "70",
-"name": "Night Life"
-},
-{
-"id": "71",
-"name": "Another World Perspective"
-},
-{
-"id": "72",
-"name": "Alone on the Forest"
-},
-{
-"id": "73",
-"name": "The Blue Sky"
-},
+     }
     ];
   })
