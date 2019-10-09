4bf0d8c809d470e5aeca0d190661408259814e42
Mayis95 <aramayisorgov95@gmail.com>
2019-10-09 04:23:23 -0700

    Payment final

--- src/routers/orderRouter.js
+++ src/routers/orderRouter.js
@@ -21,11 +21,9 @@ orderRouter.post('/order', auth, async (req, res) => {
     order.userContactInfo = req.user.contactInfo
     try{
         await order.save()
-        const url = `https://ipay.arca.am/payment/rest/register.do?userName=${process.env.PAYMENT_LOGIN}&password=${process.env.PAYMENT_PASSWORD}&returnUrl=http://www.amzbamboo.com/order/callback&amount=1000&orderNumber=${order._id}&currency=051`
-        console.log("Request: " + url)
+        const url = `https://ipay.arca.am/payment/rest/register.do?userName=${process.env.PAYMENT_LOGIN}&password=${process.env.PAYMENT_PASSWORD}&returnUrl=http://www.amzbamboo.com/order/callback&amount=${req.body.paymentInfo.amount}&orderNumber=${order._id}&currency=840`
         const data = await request(url)
         if(data.errorCode){
-            console.log("Response data" + data)
             return res.status(400).send({error: data.errorMessage})
         }
         res.send(data)
@@ -53,7 +51,6 @@ orderRouter.get('/order/callback', async (req, res) => {
         const orderID = req.query.orderId
         const url = `https://ipay.arca.am/payment/rest/getOrderStatusExtended.do?userName=${process.env.PAYMENT_LOGIN}&password=${process.env.PAYMENT_PASSWORD}&orderId=${orderID}`
         const orderStatusData = JSON.parse(await request(url))
-        console.log(JSON.stringify(orderStatusData))
         if(orderStatusData.orderStatus === 2){
             const order = await Order.findOne({_id: orderStatusData.orderNumber})
             if(!order){
@@ -61,31 +58,31 @@ orderRouter.get('/order/callback', async (req, res) => {
             }
             order.status = "accepted"
             await order.save()
-            // const mailcfg = {
-            //     to: process.env.SENDER_EMAIL_ADDRESS,
-            //     from: process.env.SENDER_EMAIL_ADDRESS,
-            //     subject: `${req.user.name} ${req.user.lastName} has sent an order`,
-            //     text: `${req.user.name} ${req.user.lastName} has sent an order`,
-            //     html: `<h1>New order from ${req.user.name} ${req.user.lastName}</h1><br>
-            //             <h3>Order details:</h3>
-            //             <ul>
-            //                 <li>User ID: ${order.ID}</li>
-            //                 <li>Contact Info: <ul>
-            //                     <li>Email for refunds: ${order.userContactInfo.emailForRefunds}</li>
-            //                     <li>Skype, Viber or WhatsApp: ${order.userContactInfo.skypeViberWhatsApp}</li>
-            //                     <li>Facebook link: ${order.userContactInfo.facebookLink}</li>
-            //                 </ul>
-            //                 <li>Product link: ${order.productLink}</li>
-            //                 <li>Buyings per day: ${order.buyingsPerDay}</li>
-            //                 <li>Item price: ${order.itemPrice}</li>
-            //                 <li>Total buying summary: ${order.totalBuyingSummary}</li>
-            //                 <li>Additional info: ${order.additionalInfo}</li>
-            //             </ul>`
-            // }
-            // sgMail.send(mailcfg)
-            res.redirect('/reportsTable')
+            const mailcfg = {
+                to: process.env.SENDER_EMAIL_ADDRESS,
+                from: process.env.SENDER_EMAIL_ADDRESS,
+                subject: `${req.user.name} ${req.user.lastName} has sent an order`,
+                text: `${req.user.name} ${req.user.lastName} has sent an order`,
+                html: `<h1>New order from ${req.user.name} ${req.user.lastName}</h1><br>
+                        <h3>Order details:</h3>
+                        <ul>
+                            <li>User ID: ${order.ID}</li>
+                            <li>Contact Info: <ul>
+                                <li>Email for refunds: ${order.userContactInfo.emailForRefunds}</li>
+                                <li>Skype, Viber or WhatsApp: ${order.userContactInfo.skypeViberWhatsApp}</li>
+                                <li>Facebook link: ${order.userContactInfo.facebookLink}</li>
+                            </ul>
+                            <li>Product link: ${order.productLink}</li>
+                            <li>Buyings per day: ${order.buyingsPerDay}</li>
+                            <li>Item price: ${order.itemPrice}</li>
+                            <li>Total buying summary: ${order.totalBuyingSummary}</li>
+                            <li>Additional info: ${order.additionalInfo}</li>
+                        </ul>`
+            }
+            sgMail.send(mailcfg)
+            return res.status(200).send({status: 'Success'})
         }
-        res.send("ERROR")
+        return res.status(400).send({error: orderStatusData.errorMessage})
     } catch(e){
         console.log(e)
         res.status(500).send({error: e.message})
