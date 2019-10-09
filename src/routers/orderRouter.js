65778229c0cd4247e7e48169588f1853ba4cf5c9
Mayis95 <aramayisorgov95@gmail.com>
2019-10-09 21:40:50 +0400

    Payment with console.log

--- src/routers/orderRouter.js
+++ src/routers/orderRouter.js
@@ -22,10 +22,12 @@ orderRouter.post('/order', auth, async (req, res) => {
     try{
         await order.save()
         const url = `https://ipay.arca.am/payment/rest/register.do?userName=${process.env.PAYMENT_LOGIN}&password=${process.env.PAYMENT_PASSWORD}&returnUrl=http://www.amzbamboo.com/order/callback&amount=${req.body.paymentInfo.amount}&orderNumber=${order._id}&currency=840`
+        console.log("Request url: " + url)
         const data = await request(url)
         if(data.errorCode){
             return res.status(400).send({error: data.errorMessage})
         }
+        console.log("Response data: " + data)
         res.send(data)
     } catch(e) {
         console.log(e)
@@ -50,7 +52,9 @@ orderRouter.get('/order/callback', async (req, res) => {
     try{
         const orderID = req.query.orderId
         const url = `https://ipay.arca.am/payment/rest/getOrderStatusExtended.do?userName=${process.env.PAYMENT_LOGIN}&password=${process.env.PAYMENT_PASSWORD}&orderId=${orderID}`
+        console.log('Status check url: ' + url)
         const orderStatusData = JSON.parse(await request(url))
+        console.log(JSON.stringify(orderStatusData))
         if(orderStatusData.orderStatus === 2){
             const order = await Order.findOne({_id: orderStatusData.orderNumber})
             if(!order){
