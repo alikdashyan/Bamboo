944945e99fd5577c98727a3f5bb7c1701f0a0790
Mayis95 <aramayisorgov95@gmail.com>
2020-03-24 22:41:51 +0300

    JSON translations moved to public/assets

--- src/routers/textRouter.js
+++ src/routers/textRouter.js
@@ -4,43 +4,43 @@ const fs = require('fs');
 const path = require('path');
 
 const router = express.Router();
-const ruTextFilePath = path.join(__dirname, '..', 'data', 'textDataRu.json');
-const engTextFilePath = path.join(__dirname, '..', 'data', 'textDataEng.json');
+const ruTextFilePath = path.join(__dirname, '..', '..', 'public', 'assets', 'textDataRu.json');
+const engTextFilePath = path.join(__dirname, '..', '..', 'public', 'assets', 'textDataEng.json');
 
 router.get('/readAll/:lang', (req, res) => {
-    try{
-        if(!req.params.lang && req.params.lang!=='en' && req.params.lang !== 'ru'){
-            return res.status(400).send({error: 'Invalid request'});
+    try {
+        if (!req.params.lang && req.params.lang !== 'en' && req.params.lang !== 'ru') {
+            return res.status(400).send({ error: 'Invalid request' });
         }
-        const filePath = req.params.lang === 'ru'? ruTextFilePath: engTextFilePath;
+        const filePath = req.params.lang === 'ru' ? ruTextFilePath : engTextFilePath;
         const data = JSON.parse(fs.readFileSync(filePath));
         res.send(data);
-    } catch(e){
+    } catch (e) {
         console.log(e);
-        res.status(500).send({error: e.message});
+        res.status(500).send({ error: e.message });
     }
 });
 
 router.patch('/update/:lang/:id', auth, (req, res) => {
-    if(req.user.kind !== 'admin'){
-        return res.status(400).send({error: "You dont have admin privileges"})
+    if (req.user.kind !== 'admin') {
+        return res.status(400).send({ error: "You dont have admin privileges" })
     };
-    if(!req.params.lang || (req.params.lang!=='en' && req.params.lang !== 'ru')){
-        return res.status(400).send({error: 'Invalid request'});
+    if (!req.params.lang || (req.params.lang !== 'en' && req.params.lang !== 'ru')) {
+        return res.status(400).send({ error: 'Invalid request' });
     }
-    const filePath = req.params.lang === 'ru'? ruTextFilePath: engTextFilePath;
-    try{
+    const filePath = req.params.lang === 'ru' ? ruTextFilePath : engTextFilePath;
+    try {
         const data = JSON.parse(fs.readFileSync(filePath));
         const item = data[req.params.id];
-        if(item){
+        if (item) {
             Object.assign(item, req.body);
             fs.writeFileSync(filePath, JSON.stringify(data));
             return res.send(item);
         }
-        res.status(404).send({error: 'Text item not found'});
-    } catch(e) {
+        res.status(404).send({ error: 'Text item not found' });
+    } catch (e) {
         console.log(e);
-        res.status(500).send({error: e.message});
+        res.status(500).send({ error: e.message });
     }
 });
 
